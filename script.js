36d1a16259018bdb13b9de8d87fd38925112bd95
Durga Prasad <durgaprasadnakka66@gmail.com>
2025-12-25 12:23:48 +0530

    Initial task manager frontend

---
+++ script.js
@@ -0,0 +1,22 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let taskText = input.value;
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = `
+        ${taskText}
+        <button onclick="removeTask(this)">Delete</button>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+}
+
+function removeTask(button) {
+    button.parentElement.remove();
+}
